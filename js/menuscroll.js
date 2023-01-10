62aeb450bed32dd7bf78f3ec55fb7f9eb00e658a
denimytmp@gmail.com <my13pass11gith2022>
2023-01-11 05:59:41 +0700

    055911012023

--- js/menuscroll.js
+++ js/menuscroll.js
@@ -1,16 +1,34 @@
+// $(window).scroll(function () {
+//   var wScroll = $(this).scrollTop();
+//   if (wScroll >= 5) {
+//     $("nav")
+//       .css({
+//         transition: "all 1s",
+//       })
+//       .addClass("navbg");
+//   } else {
+//     $("nav")
+//       .css({
+//         transition: "all 1s",
+//       })
+//       .removeClass("navbg");
+//   }
+// });
 $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
-  if (wScroll >= 25) {
+  if (wScroll >= 5) {
     $("nav")
       .css({
         transition: "all 1s",
       })
       .addClass("navbg");
+    $(".menu ul li a").removeClass("color");
   } else {
     $("nav")
       .css({
         transition: "all 1s",
       })
       .removeClass("navbg");
+    $(".menu ul li a").addClass("color");
   }
 });
