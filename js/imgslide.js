3c851ad42eb3607ac6cf05bd2c9301355e7ecf75
denimytmp@gmail.com <my13pass11gith2022>
2023-01-12 13:02:38 +0700

    130212012023

--- js/imgslide.js
+++ js/imgslide.js
@@ -1,37 +1,3 @@
-// var myIndex = 0;
-// carousel();
-
-// function carousel() {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";
-//   }
-//   myIndex++;
-//   if (myIndex > x.length) {
-//     myIndex = 1;
-//   }
-//   x[myIndex - 1].style.display = "flex";
-//   setTimeout(carousel, 2000);
-// }
-
-// // var myIndex = 0;
-// // carousel();
-
-// // function carousel() {
-// //   var i;
-// //   var x = document.getElementsByClassName("mySlides");
-// //   for (i = 0; i < x.length; i++) {
-// //     x[i].classList.remove("show");
-// //   }
-// //   myIndex++;
-// //   if (myIndex > x.length) {
-// //     myIndex = 1;
-// //   }
-// //   x[myIndex - 1].classList.add("show");
-// //   setTimeout(carousel, 1000);
-// // }
-
 let slideIndex = 1;
 let intervalId;
 
@@ -53,7 +19,7 @@ function currentSlide(n) {
 function startAutoSlide() {
   intervalId = setInterval(() => {
     showSlides((slideIndex += 1));
-  }, 2000);
+  }, 5000);
 }
 
 function showSlides(n) {
