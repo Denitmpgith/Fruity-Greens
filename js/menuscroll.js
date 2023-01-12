40b8392b6050e68c8d15b28f7d1dad641b4b0269
denimytmp@gmail.com <my13pass11gith2022>
2023-01-13 01:11:10 +0700

    011113012023

--- js/menuscroll.js
+++ js/menuscroll.js
@@ -1,47 +1,18 @@
-// $(window).scroll(function () {
-//   var wScroll = $(this).scrollTop();
-//   if (wScroll >= 5) {
-//     $("nav")
-//       .css({
-//         transition: "all 1s",
-//       })
-//       .addClass("navbg");
-//   } else {
-//     $("nav")
-//       .css({
-//         transition: "all 1s",
-//       })
-//       .removeClass("navbg");
-//   }
-// });
-// $(window).scroll(function () {
-//   var wScroll = $(this).scrollTop();
-//   if (wScroll >= 5) {
-//     $("nav")
-//       .css({
-//         transition: "all 1s",
-//       })
-//       .addClass("navbg");
-//     $(".menu ul li a").addClass("color2");
-//     $(".menu ul li a").removeClass("color1");
-//   } else {
-//     $("nav")
-//       .css({
-//         transition: "all 1s",
-//       })
-//       .removeClass("navbg");
-//     $(".menu ul li a").addClass("color1");
-//     $(".menu ul li a").removeClass("color2");
-//   }
-// });
 $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
   $("nav").css({ transition: "all 0.5s" });
-  if (wScroll >= 5) {
-    $("nav").addClass("navbg");
-    $(".menu ul li a").addClass("color2").removeClass("color1");
-  } else {
-    $("nav").removeClass("navbg");
-    $(".menu ul li a").addClass("color1").removeClass("color2");
+  if (wScroll < 5) {
+    $("nav").removeClass("navbg").removeClass("navbg2").removeClass("navbg3");
+    $(".menu ul li a").addClass("color1").removeClass("color2").removeClass("color3");
+  } else if (wScroll >= 5 && wScroll < 500) {
+    $("nav").removeClass("navbg2").removeClass("navbg3").addClass("navbg");
+    $(".menu ul li a").addClass("color2").removeClass("color1").removeClass("color3");
+  } else if (wScroll >= 501 && wScroll < 1500) {
+    $("nav").removeClass("navbg").removeClass("navbg3").addClass("navbg2");
+    $(".menu ul li a").addClass("color3").removeClass("color1").removeClass("color2");
+  } else if (wScroll >= 1501) {
+    $("nav").removeClass("navbg").removeClass("navbg2").addClass("navbg3");
+    $(".menu ul li a").addClass("color1").removeClass("color2").removeClass("color3");
   }
+  // console.log(wScroll);
 });
