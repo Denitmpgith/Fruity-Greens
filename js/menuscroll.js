bde7af734284323f4acf9cdb9f69626618472a84
denimytmp@gmail.com <my13pass11gith2022>
2023-01-13 01:52:37 +0700

    015213012023

--- js/menuscroll.js
+++ js/menuscroll.js
@@ -4,10 +4,10 @@ $(window).scroll(function () {
   if (wScroll < 5) {
     $("nav").removeClass("navbg").removeClass("navbg2").removeClass("navbg3");
     $(".menu ul li a").addClass("color1").removeClass("color2").removeClass("color3");
-  } else if (wScroll >= 5 && wScroll < 500) {
+  } else if (wScroll >= 5 && wScroll < 750) {
     $("nav").removeClass("navbg2").removeClass("navbg3").addClass("navbg");
     $(".menu ul li a").addClass("color2").removeClass("color1").removeClass("color3");
-  } else if (wScroll >= 501 && wScroll < 1500) {
+  } else if (wScroll >= 751 && wScroll < 1500) {
     $("nav").removeClass("navbg").removeClass("navbg3").addClass("navbg2");
     $(".menu ul li a").addClass("color3").removeClass("color1").removeClass("color2");
   } else if (wScroll >= 1501) {
